31b8bd864aab79b9c4db401905687a786a3fbdaa
shungo-555 <109016881+shungo-555@users.noreply.github.com>
2026-05-06 09:53:03 +0900

    fix: Service Workerのキャッシュ更新（v2）

--- sw.js
+++ sw.js
@@ -1,16 +1,32 @@
-const CACHE_NAME = 'waku-waku-timer-v1';
+const CACHE_NAME = 'waku-waku-timer-v2';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './main.js',
-  './manifest.json'
+  './manifest.json',
+  './icon.svg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
